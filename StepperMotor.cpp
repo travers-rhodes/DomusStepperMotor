ad7c6df2338cc78a12ad203677de7bfe56483f6c
Travers Rhodes <traversr@andrew.cmu.edu>
2018-07-26 15:20:11 -0400

    I really really really really really like you

--- StepperMotor.cpp
+++ StepperMotor.cpp
@@ -1,25 +1,94 @@
 #include "StepperMotor.h"
 #include "Arduino.h"
 
-// the fastest we send ticks to motor
-const int MIN_TICK_PERIOD_MICROS = 400;
 const int PULSE_LENGTH_MICROS = 200;
+
+//
+// Helper Methods
+//
+// returns speed in ticks per micro
+double GetTargetSpeed(int current_state_in_ticks,
+  int destination_in_ticks, 
+  unsigned long current_time_millis, 
+  unsigned long destination_time_millis,
+  double max_motor_speed_ticks_per_micro)
+{
+  if (current_state_in_ticks == destination_in_ticks)
+  {
+    // we're already there, so no need to move
+    return 0;
+  }
+  double ticks_to_move = destination_in_ticks - current_state_in_ticks;
+  unsigned long time_remaining_millis = destination_time_millis - current_time_millis;
+  // compute the naive speed, and then truncate as needed 
+  // based on robot speed limits
+  double speed = (ticks_to_move / time_remaining_millis) / 1000.0;
+
+  if (abs(speed) > max_motor_speed_ticks_per_micro
+       || destination_time_millis < current_time_millis)
+  {
+    if (ticks_to_move > 0)
+    {
+      speed = max_motor_speed_ticks_per_micro;
+    }
+    else
+    {
+      speed = -max_motor_speed_ticks_per_micro;
+    }
+  }
+
+  return speed;
+}
+
+// get the new speed based on current speed, target speed, and 
+// acceleration capabilities
+double GetNewSpeed(double current_speed_ticks_per_micro,
+      double target_speed_ticks_per_micro,
+      double motor_acceleration_ticks_per_micro_squared,
+      unsigned long micros_since_last_speed_update)
+{
+  // check how much we can accelerate in the alloted time
+  double allowed_speed_change = motor_acceleration_ticks_per_micro_squared * micros_since_last_speed_update;
+  // if we can accelerate to the target speed, do that
+  if (abs(target_speed_ticks_per_micro - current_speed_ticks_per_micro) < allowed_speed_change)
+  {
+    return target_speed_ticks_per_micro;
+  }
+  // otherwise, do max acceleration in the required direction
+  if (target_speed_ticks_per_micro > current_speed_ticks_per_micro)
+  {
+    return current_speed_ticks_per_micro + allowed_speed_change;
+  }
+  return current_speed_ticks_per_micro - allowed_speed_change;
+}
+    
+
+//
+// Class Definition
+//
 
 StepperMotor::StepperMotor()
 {
   //default constructor does nothing. you shouldn't use this object constructed this way, but I'm new to cpp...
 }
 
-StepperMotor::StepperMotor(int ticks_per_revolution, float gear_ratio, float min_angle, float max_angle, int step_pin, int direction_pin)
+StepperMotor::StepperMotor(int ticks_per_revolution, 
+    float gear_ratio, 
+    int step_pin, 
+    int direction_pin, 
+    double max_motor_speed_ticks_per_micro, 
+    double motor_acceleration_ticks_per_micro_squared)
 {
   _ticks_per_revolution = ticks_per_revolution;
   _gear_ratio = gear_ratio;
-  _min_bound_in_ticks = ConvertAngleToTicks(min_angle);
-  _max_bound_in_ticks = ConvertAngleToTicks(max_angle);
   _step_pin = step_pin;
   _direction_pin = direction_pin;
+  _max_motor_speed_ticks_per_micro = max_motor_speed_ticks_per_micro;
+  _motor_acceleration_ticks_per_micro_squared = motor_acceleration_ticks_per_micro_squared;
+
   _is_destination_active = false; 
   _motor_direction = -1;
+  _current_speed_ticks_per_micro = 0;
 }
 
 void StepperMotor::Calibrate(float current_angle)
@@ -31,50 +100,65 @@ void StepperMotor::Calibrate(float current_angle)
 void StepperMotor::SetTarget(unsigned long destination_time_millis, float destination_angle)
 {
   _is_destination_active = false;
-  _destination_time_millis = destination_time_millis;
   _destination_in_ticks = ConvertAngleToTicks(destination_angle);
-  //Serial.print(_destination_in_ticks);
-  _last_motor_uptick_micros = micros();
-  // set the motor direction as needed
-  int abs_ticks_to_move;
-  if (_current_state_in_ticks == _destination_in_ticks)
+  _time_of_last_speed_update_micros = micros();
+  
+  unsigned long current_time_millis = millis();
+
+  _target_speed_ticks_per_micro =
+    GetTargetSpeed(_current_state_in_ticks,
+      _destination_in_ticks,
+      current_time_millis,
+      destination_time_millis,
+      _max_motor_speed_ticks_per_micro);
+
+  _is_destination_active = true;
+}
+
+// update the current speed of the motor
+void StepperMotor::UpdateSpeed()
+{
+  // nothing to do if the motor is off
+  if (!_is_destination_active)
   {
-    // we're already there, so no need to move
     return;
   }
-  
-  if (_current_state_in_ticks >= _destination_in_ticks)
+ 
+  unsigned long cur_micros = micros();
+  unsigned long micros_since_last_speed_update =  
+    cur_micros -  _time_of_last_speed_update_micros;
+
+  // we need checks like this in case micros overflowed
+  if (micros_since_last_speed_update < 0)
   {
-    if (_motor_direction != -1) {
-      digitalWrite(_direction_pin, HIGH);
-      _motor_direction = -1;
-    }
-    //Serial.print("going right");
-    abs_ticks_to_move = _current_state_in_ticks - _destination_in_ticks;
+    micros_since_last_speed_update = 0;
+  }
+  
+  _current_speed_ticks_per_micro = 
+    GetNewSpeed(_current_speed_ticks_per_micro,
+      _target_speed_ticks_per_micro,
+      _motor_acceleration_ticks_per_micro_squared,
+      micros_since_last_speed_update);
+
+  // if you're trying to go slower than 1 tick per 1000 seconds,
+  // we'll have you go at one tick per 1000 seconds.
+  if (abs(_current_speed_ticks_per_micro) < 1e-9) 
+  {
+    _motor_tick_period_micros = 1e9;
   }
   else
   {
-    if (_motor_direction != 1) {
-      digitalWrite(_direction_pin, LOW);
-      _motor_direction = 1;
-    }
-    //Serial.print("going left");
-    abs_ticks_to_move = _destination_in_ticks - _current_state_in_ticks;
-  } 
-  unsigned long current_time_millis = millis();
-  if (current_time_millis >= _destination_time_millis)
-  {
-    //Serial.print("we can't go back in time");
-    return;
+    _motor_tick_period_micros = abs(1.0/_current_speed_ticks_per_micro);
   }
-   
-  _motor_tick_period_micros = (unsigned long)((_destination_time_millis - current_time_millis) * 1000)/abs_ticks_to_move;
-  if (_motor_tick_period_micros < MIN_TICK_PERIOD_MICROS) {
-    _motor_tick_period_micros = MIN_TICK_PERIOD_MICROS;    
-  }
-  //Serial.print(_motor_tick_period_micros);
-  _is_destination_active = true;
-  //Serial.print(_is_destination_active);
+  
+  //Serial.print(_target_speed_ticks_per_micro * 1e3);
+  //Serial.print(" is target ");
+  //Serial.print(_current_state_in_ticks);
+  //Serial.print(" is position\n");
+
+  UpdateMotorDirectionPin();
+
+  _time_of_last_speed_update_micros = cur_micros;
 }
 
 void StepperMotor::UpdatePosition()
@@ -83,30 +167,60 @@ void StepperMotor::UpdatePosition()
     // if we're not trying to go anywhere, don't do anything
     return;
   }
-  // we've taken long enough, so stop moving
-  //if (millis() > _destination_time_millis)
-  //{
-  //  _is_destination_active = false;
-  //}
-  if (_current_state_in_ticks >= _destination_in_ticks)
+  if (_current_state_in_ticks == _destination_in_ticks)
   {
+    // turn off the motor 
+    // and therefore also log that the current speed is 0
     _is_destination_active = false;
+    _current_speed_ticks_per_micro = 0;
+    return;
   }
-  if (_is_stepper_signal_high && (micros() > _last_motor_uptick_micros + PULSE_LENGTH_MICROS))
+
+  unsigned long cur_micros = micros();
+  if (_is_stepper_signal_high 
+        && (cur_micros > _last_motor_uptick_micros + PULSE_LENGTH_MICROS 
+            || cur_micros < _last_motor_uptick_micros))
   {
     digitalWrite(_step_pin, LOW);
     //Serial.print("Going low\n");
     _is_stepper_signal_high = false;
     return;
   }
-  if (!_is_stepper_signal_high && micros() > _last_motor_uptick_micros + _motor_tick_period_micros)
+  if (!_is_stepper_signal_high 
+        && (cur_micros > _last_motor_uptick_micros + _motor_tick_period_micros
+            || cur_micros < _last_motor_uptick_micros))
   {
     digitalWrite(_step_pin, HIGH);
     //Serial.print("Going high\n");
     _is_stepper_signal_high = true;
-    _last_motor_uptick_micros = micros();
+    _last_motor_uptick_micros = cur_micros;
     //update the current state
     _current_state_in_ticks += _motor_direction;
+  }
+}
+
+//
+// Private methods
+//
+
+
+// update the motor direction pin if needed
+// this function only changes the pin if
+// _current_speed_ticks_per_micro is requesting
+// a different direction from _motor_direction
+void StepperMotor::UpdateMotorDirectionPin()
+{
+  if (_current_speed_ticks_per_micro > 0
+        && _motor_direction != 1)
+  {
+      digitalWrite(_direction_pin, LOW);
+      _motor_direction = 1;
+  }
+  else if (_current_speed_ticks_per_micro < 0
+             && _motor_direction != -1)
+  {
+      digitalWrite(_direction_pin, HIGH);
+      _motor_direction = -1;
   }
 }
 
--- StepperMotor.h
+++ StepperMotor.h
@@ -5,17 +5,25 @@ class StepperMotor
 {
   public:
     StepperMotor();
-    StepperMotor(int ticks_per_revolution, float gear_ratio, float min_angle, float max_angle, int step_pin, int direction_pin);
+    StepperMotor(int ticks_per_revolution, 
+      float gear_ratio, 
+      int step_pin, 
+      int direction_pin,
+      double max_motor_speed_ticks_per_micro,
+      double motor_acceleration_ticks_per_micro_squared);
     void Calibrate(float current_angle);
     // set the target for this motor.
     // the target has two components:
     // the desired destination angle and the time we should get there
     // the time corresponds to the time that millis() would return on the arduino
     void SetTarget(unsigned long destination_time_millis, float destination_angle);
+    void UpdateSpeed();
     // Check if it's time to take a motor tick, and if so, take a tick
     void UpdatePosition();
   private:
+    void UpdateMotorDirectionPin();
     int ConvertAngleToTicks(float angle);
+
     //
     // Constants related to robot motor mechanics
     // 
@@ -23,19 +31,18 @@ class StepperMotor
     // This is a characteristic of the motor driver
     int _ticks_per_revolution;
     float _gear_ratio;
-    int _min_bound_in_ticks;
-    int _max_bound_in_ticks;
+
     //
     // variables related to user request to move motor
     //
-    // when to arrive at destination (in millis since start of arduino)
-    unsigned long _destination_time_millis;
     // target destination (in motor ticks. 0 is ref config)
+    // only guaranteed to exist if _is_destination_active
     int _destination_in_ticks;
     // Whether or not we are actively pursuing a course to the destination or not
     // This can be set to false in order to immediately stop the motor (should also be false during calibration)
     // Setting this to false is basically like setting _destination_in_ticks to NULL
     bool _is_destination_active;
+
     //
     // variables related to current motor state
     //
@@ -45,15 +52,31 @@ class StepperMotor
     // last time motor moved (in micros since start of arduino). 
     // micros() overflows every 70 minutes, but like... that's a long time, right?
     unsigned long _last_motor_uptick_micros;
+
     //
     // Variables related to commands to move motor
     //
     // which direction we want to be spinning the motor (ie: left or right)
     int _motor_direction;
+    // this number should be probably no more than around
+    // 1 tick / 200 micros
+    double _max_motor_speed_ticks_per_micro;
+    // who knows what order of magnitude this number should be, but it
+    // says how much we let the robot arm accelerate.
+    double _motor_acceleration_ticks_per_micro_squared;
+    // (a very small double. The inverse of _motor_tick_period_micros)
+    double _current_speed_ticks_per_micro;
+    // micros clock time when we last updated the motor's speed
+    // needed in finite difference updating for acceleration
+    unsigned long _time_of_last_speed_update_micros;
+    // (a very small double)
+    // this is the naive speed the arm would want to go if there were no smoothing.
+    double _target_speed_ticks_per_micro;
     // how many micros between motor ticks in order to reach destination on time
     unsigned long _motor_tick_period_micros;
     // are we currently sending a high (or low) pulse to the stepper motor
     bool _is_stepper_signal_high;
+
     //
     // Arduino communication pins
     //
